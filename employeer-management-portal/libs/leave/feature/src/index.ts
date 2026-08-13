1c058d5aced4111593853438d38f93522209015c
Rina Jasharaj <75223541+rinajasharaj@users.noreply.github.com>
2026-08-13 20:29:43 +0200

    Leave feature reads employee data directly - needed employee name for leave approval, domain-api didn't expose it yet, will fix later

--- employeer-management-portal/libs/leave/feature/src/index.ts
+++ employeer-management-portal/libs/leave/feature/src/index.ts
@@ -1,3 +1,3 @@
 export * from './lib/leave-feature/leave-feature';
-// import * as something from '@employeer-management-portal/employees-data-access';
+import * as employeesDataAccess from '@employeer-management-portal/employees-data-access';
 
